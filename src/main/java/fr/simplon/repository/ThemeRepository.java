172feffdb1024a3bf7ade8dfacc789dfcd43c979
Tony Deride <tony.deride.lp4@gmail.com>
2020-02-03 10:09:08 +0100

    rangement

--- src/main/java/fr/simplon/repository/ThemeRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.simplon.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import fr.simplon.model.Theme;
-
-public interface ThemeRepository extends CrudRepository<Theme , Integer> {}
