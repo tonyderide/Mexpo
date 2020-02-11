275494699d32f5338519ced950e01181a92479af
tony deride <tony.deride.lp4@gmail.com>
2020-02-11 16:29:01 +0100

    autocompletion

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+
+import { Pipe, PipeTransform } from '@angular/core';
+import { Directive } from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -18,15 +21,24 @@ import { CarteRegionComponent } from './carte-region/carte-region.component';
 import { ListeDeroulanteEnCascadeComponent } from './liste-deroulante-en-cascade/liste-deroulante-en-cascade.component';
 import { TableauResultatMuseeComponent } from './tableau-resultat-musee/tableau-resultat-musee.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/';
 import { MatTableModule } from '@angular/material/table';
 import { FormsModule } from '@angular/forms';
 import { ImageMuseesComponent } from './image-musees/image-musees.component';
 import { ImageThemesComponent } from './image-themes/image-themes.component';
+import {
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule } from "@angular/material";
+import {MatInputModule} from '@angular/material';
+import { MatAutocompleteModule } from "@angular/material/";
 
 @NgModule({
   declarations: [
+
     AppComponent,
     NavbarComponent,
     SearchMuseeComponent,
@@ -44,7 +56,7 @@ import { ImageThemesComponent } from './image-themes/image-themes.component';
     TableauResultatMuseeComponent,
     ImageMuseesComponent,
     ImageThemesComponent,
-  ],
+],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -53,7 +65,17 @@ import { ImageThemesComponent } from './image-themes/image-themes.component';
     AppRoutingModule,
     MatPaginatorModule,
     MatTableModule,
-  ],
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- front/src/app/searchbar/search-musee.component.ts
+++ front/src/app/searchbar/search-musee.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from "rxjs";
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { Musee} from '../models/musee'
+import { MuseeService } from "../musee.service";
+
 
 @Component({
   selector: 'app-search-musee',
   templateUrl: './search-musee.component.html',
   styleUrls: ['./search-musee.component.scss']
 })
+
 export class SearchMuseeComponent implements OnInit {
+  listMusee$: Musee[];
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private museeService: MuseeService) {
   }
 
+  ngOnInit(): void {
+    this.recupeMuseeList();
+  }
+
+  recupeMuseeList(): void {
+    this.museeService.getMuseesList()
+      .subscribe((musee$: any) => {
+        this.listMusee$ = musee$;
+        console.log(this.listMusee$);
+      });
+  };
 }
+// ngOnInit(): void {
+//   // console.log(this.museeService.getMuseesList());
+
+//   this.listMusee$= this.searchTerms.pipe(
+//     debounceTime(300),
+//     distinctUntilChanged(),
+//     switchMap((term:string) => this.museeService.getMuseeById(term))
+//     );
+//   console.log(this.listMusee$);
+// }
+// displayfonction(subject){
+//   return subject ? subject : undefined;
+// }
+
+// }
+// search(term):void {
+//   this.searchTerms.next(term);
+//   console.log(term);
+// }
+
