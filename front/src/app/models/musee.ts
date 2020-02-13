9319451758799cf63ac34389e7c53ef90338d337
tony deride <tony.deride.lp4@gmail.com>
2020-02-13 15:12:33 +0100

    search barre qui fonctionne

--- front/src/app/models/musee.ts
+++ front/src/app/models/musee.ts
@@ -7,13 +7,13 @@ export class Musee {
     geolocalisation: string;
     description: string;
 
-    constructor(id: string, nom: string, adr: string, vil: string, site: string, geo: string, des: string) {
-        this.idMusee = id;
-        this.nomMusee = nom;
-        this.adresse = adr;
-        this.ville = vil;
-        this.siteWeb = site;
-        this.geolocalisation = geo;
-        this.description = des;
-    }
+    // constructor(id: string, nom: string, adr: string, vil: string, site: string, geo: string, des: string) {
+    //     this.idMusee = id;
+    //     this.nomMusee = nom;
+    //     this.adresse = adr;
+    //     this.ville = vil;
+    //     this.siteWeb = site;
+    //     this.geolocalisation = geo;
+    //     this.description = des;
+    // }
 }
--- front/src/app/searchbar/search-musee.component.ts
+++ front/src/app/searchbar/search-musee.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from "rxjs";
 import {debounceTime, distinctUntilChanged, map, startWith, switchMap} from 'rxjs/operators';
-import { Musee} from '../models/musee'
+import { Musee } from '../models/musee'
 import { MuseeService } from "../musee.service";
 import {FormControl} from "@angular/forms";
-
+import { options } from "../searchbar/listNomMusee"
 
 @Component({
   selector: 'app-search-musee',
@@ -13,35 +13,41 @@ import {FormControl} from "@angular/forms";
 })
 
 export class SearchMuseeComponent implements OnInit {
-     listMusee$: Musee[];
-     private searchTerms = new Subject<string>();
-     myControl: FormControl = new FormControl();
-     //filteredOption : Observable<string[]>;
-     displayFn(subject){ return subject ? subject.nomMusee : undefined; }
+  listMusee$: Musee[];
+  // private searchTerms = new Subject<string>();
+  myControl: FormControl = new FormControl();
+  filteredOption : Observable<string[]>;
+  listNomMusee= options;
+  nomMusee: string;
 
   constructor(private museeService: MuseeService) {
   }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.recupeMuseeList();
-    // this.filteredOption = this.myControl.valueChanges.pipe(
-    //     startWith(''),
-    //     map(val => this.filter(val))
-    //   );
-    // console.log(this.filteredOption);
+    this.filteredOption = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
   }
-  // filter(val: string): string[]{
-  //   return this.listMusee$.map(musee => musee.nomMusee).filter(option =>
-  //   option.toLowerCase().includes(val.toLowerCase()));
-  // }
+    private _filter(value: string): string[] {
+      const filterValue = value.toLowerCase();
+
+      return this.listNomMusee.filter(option => option.toLowerCase().includes(filterValue));
+    }
 
   recupeMuseeList(): void {
     this.museeService.getMuseesList()
-      .subscribe((musee$: any) => {
-        this.listMusee$ = musee$;
-        console.log(this.listMusee$);
+      .subscribe((musee$: Musee[]) => { this.listMusee$ = musee$;
       });
-  };
+
+
+  }
+
+  displayFn(subject) {return subject ? subject.nomMusee : undefined;}
+
+
 
 
 }
